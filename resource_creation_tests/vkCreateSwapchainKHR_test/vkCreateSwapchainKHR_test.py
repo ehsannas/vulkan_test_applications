cbfe9f0a7be704fc0c1406017462807f99a204a5
Andrew Woloszyn <awoloszyn@google.com>
2016-09-22 16:13:38 -0400

    Udpated swapchain test to use the new VulkanStruct.
    Change-Id: I4ca64b38a8c634b288b64b7c2e6c6a80cd463138
    Reviewed-on: https://team-review.git.corp.google.com/50543
    Reviewed-by: David Neto <dneto@google.com>

--- resource_creation_tests/vkCreateSwapchainKHR_test/vkCreateSwapchainKHR_test.py
+++ resource_creation_tests/vkCreateSwapchainKHR_test/vkCreateSwapchainKHR_test.py
@@ -29,65 +29,45 @@ class SwapchainCreateTest(GapitTest):
         createSwapchain = require(self.next_call_of("vkCreateSwapchainKHR"))
         destroySwapchain = require(self.next_call_of("vkDestroySwapchainKHR"))
 
-        swapchain_create_info = VulkanStruct(architecture,
-                                             [UINT32_T,  # sType
-                                              POINTER,  # pNext
-                                              UINT32_T,  # flags
-                                              HANDLE,  # surface
-                                              UINT32_T,  # minImageCount
-                                              UINT32_T,  # imageFormat
-                                              UINT32_T,  # imageColorSpace
-                                              UINT32_T,  # extent.width
-                                              UINT32_T,  # extent.height
-                                              UINT32_T,  # imageArrayLayers
-                                              UINT32_T,  # imageUsage
-                                              UINT32_T,  # imageSharingMode
-                                              UINT32_T,  # queueFamilyIndexCount
-                                              POINTER,  # queueFamilyIndices
-                                              UINT32_T,  # preTransform
-                                              UINT32_T,  # compositeAlpha
-                                              UINT32_T,  # presentMode
-                                              UINT32_T,  # clipped
-                                              HANDLE,  # oldSwapchain
-                                             ])
-        sType = little_endian_bytes_to_int(
-            require(
-                createSwapchain.get_read_data(
-                    createSwapchain.hex_PCreateInfo +
-                    swapchain_create_info.get_offset_of(0), 4)))
+        def get_swapchain_create_info_member(offset, size):
+            return little_endian_bytes_to_int(
+                require(
+                    createSwapchain.get_read_data(
+                        createSwapchain.hex_PCreateInfo + offset, size)))
 
-        require_equal(sType, VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR)
+        swapchain_create_info = VulkanStruct(
+            architecture,
+            [("sType", UINT32_T),
+             ("pNext", POINTER),
+             ("flags", UINT32_T),
+             ("surface", HANDLE),
+             ("minImageCount", UINT32_T),
+             ("imageFormat", UINT32_T),
+             ("imageColorSpace", UINT32_T),
+             ("extent.width", UINT32_T),
+             ("extent.height", UINT32_T),
+             ("imageArrayLayers", UINT32_T),
+             ("imageUsage", UINT32_T),
+             ("imageSharingMode", UINT32_T),
+             ("queueFamilyIndexCount", UINT32_T),
+             ("queueFamilyIndices", POINTER),
+             ("preTransform", UINT32_T),
+             ("compositeAlpha", UINT32_T),
+             ("presentMode", UINT32_T),
+             ("clipped", UINT32_T),
+             ("oldSwapchain", HANDLE)  # oldSwapchain
+            ],
+            get_swapchain_create_info_member)
 
-        oldSwapchain = little_endian_bytes_to_int(
-            require(
-                createSwapchain.get_read_data(
-                    createSwapchain.hex_PCreateInfo +
-                    swapchain_create_info.get_offset_of(18), 8)))
-        require_equal(oldSwapchain, 0)
+        require_equal(swapchain_create_info.sType,
+                      VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR)
 
-        clipped = little_endian_bytes_to_int(
-            require(
-                createSwapchain.get_read_data(
-                    createSwapchain.hex_PCreateInfo +
-                    swapchain_create_info.get_offset_of(17), 4)))
-        require_equal(clipped, 0)
-
-        image_array_layers = little_endian_bytes_to_int(
-            require(
-                createSwapchain.get_read_data(
-                    createSwapchain.hex_PCreateInfo +
-                    swapchain_create_info.get_offset_of(9), 4)))
-        require_equal(image_array_layers, 1)
-
+        require_equal(swapchain_create_info.oldSwapchain, 0)
+        require_equal(swapchain_create_info.clipped, 0)
+        require_equal(swapchain_create_info.imageArrayLayers, 1)
         require_not_equal(0, destroySwapchain.int_Swapchain)
-
-        queue_family_index_count = little_endian_bytes_to_int(
-            require(
-                createSwapchain.get_read_data(
-                    createSwapchain.hex_PCreateInfo +
-                    swapchain_create_info.get_offset_of(12), 4)))
-        require_equal(True, (queue_family_index_count == 0 or
-                             queue_family_index_count == 2))
+        require_equal(True, (swapchain_create_info.queueFamilyIndexCount == 0 or
+                             swapchain_create_info.queueFamilyIndexCount == 2))
 
         if self.not_device(device_properties, 0x5A400000,
                            gapit_test_framework.PIXEL_C):
